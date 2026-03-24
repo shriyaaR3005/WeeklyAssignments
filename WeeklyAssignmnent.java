f43092150d32294b362404245a510fc78b2c6dfd
ShriyaaR3005 <sr4146@srmist.edu.in>
2026-03-24 22:51:32 +0530

    L1Q2 average of PCM Marks

--- WeeklyAssignmnent.java
+++ WeeklyAssignmnent.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
-public class HarryAge {
+public class AveragePCM {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int birthYear = 2000;
-        System.out.print("Enter current year: ");
-        int currentYear = sc.nextInt();
-        int age = currentYear - birthYear;
-        System.out.println("Harry's age is " + age);
+        System.out.print("Enter Maths mark: ");
+        int maths = sc.nextInt();
+        System.out.print("Enter Physics mark: ");
+        int physics = sc.nextInt();
+        System.out.print("Enter Chemistry mark: ");
+        int chemistry = sc.nextInt();
+        double average = (maths + physics + chemistry) / 3.0;
+        System.out.println("Sam's average mark in PCM is " + average);
         sc.close();
     }
 }
